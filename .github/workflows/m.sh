8ee4f5467edcd2e722ee821b1d3ea888aba61c00
HariAryal <80886798+HariAryal@users.noreply.github.com>
2022-07-08 11:50:06 +0545

    Update m.sh

--- .github/workflows/m.sh
+++ .github/workflows/m.sh
@@ -2,13 +2,13 @@
 
   set -ex
 
-  API_KEY='0337b5ea10d37288'
-  INTEGRATIONS_API_URL='https://3000-qualitiai-qualitiapi-51d2c33ux5c.ws-us51.gitpod.io'
+  API_KEY='<replace-with-api-key>'
+  INTEGRATIONS_API_URL='https://api.qualiti-dev.com'
   PROJECT_ID='3'
-  CLIENT_ID='b88a1b16791b4b176add0b80d93f69d3'
+  CLIENT_ID='dc2761a1634b1b2d1465196fadcc86b2'
   SCOPES=['"ViewTestResults"','"ViewAutomationHistory"']
-  API_URL='https://3000-qualitiai-qualitiapi-51d2c33ux5c.ws-us51.gitpod.io'
-  INTEGRATION_JWT_TOKEN='b2f7252c8a8b829ab43a1b5c23507baa65361a64943516e2b87b5f5c8a2018078a8d596023ec3e58bdd571af2a2270147ed5e4dfe08ac49047e6123345446e94011ce532fb98c4a7e706d654b06cc4456ceff2ae00740e3b4e30a7581ee29fc37d4fdf0ee9685dec952525979492decf8ba037a18085be7e38d760e71debae3673bf50505e39a0e33d70d129e42fe431baf371e6c4020dc01b6bdb7f0883fc091b312d099b3a7d0a41ed6c54c85565431b6f554e330a0694c340ea33e7af9d65ad735d701c2da5f46fd233cd1f66cedec3b566e194d4d2f31f1225a64d4b2da71ae57ecc2f897ff36dd9cf2818a6f7e3167fc6511b60ce856d37d31b516803fe8dbaed7c25371d77327a445809ec513d|07d74449e6fbffdc030a9a64a4c94245|1fe3d7bb42817dff9ede1c79d264e995'
+  API_URL='https://api.qualiti-dev.com/public/api'
+  INTEGRATION_JWT_TOKEN='764275e459449b0287ddcacaec9f2e091b0c4e079e1a3bd03f18a75fb2f298a63dead775363f6f386e82d4753a5668115112fee78c81d1e9916c89c469771cb7287b1061d276ac8b0bcaa56c5111f19b158dca823f8e6bef08cec2f90d9de6338dfdcfc11868255d32cbf78d2a962e6b40a6a459ec33029f5a76c8f455dfab2bcb89ad8b0083ba16cc6c68c198144fde82779f034598ceabc16c56fc279121c76da6affb7cb289cf58ed86f0f2d7d3ba67801b42e0c8d1033856277d4ce7863e432813ecd4b63d0564f120606b6da4cbdb3b71e4d2b74d1cc897d6455fc043521c44b16211d44bc5355b81c469609def29507f0b138c48ed160e67ee8c46d072a9396cfcc322a62072fd7e05136bf015|8a5b25759237711a067526ee706156c5|ed7ac1244e0e445b948614e0db570612'
 
   sudo apt-get update -y
   sudo apt-get install -y jq
@@ -17,16 +17,16 @@
   TEST_RUN_ID="$( \
     curl -X POST -G ${INTEGRATIONS_API_URL}/integrations/github/${PROJECT_ID}/events \
       -d 'token='$INTEGRATION_JWT_TOKEN''\
-      -d 'triggerType=Deploy'\
+      -d 'triggeredBy=Deploy'\
     | jq -r '.test_run_id')"
 
   AUTHORIZATION_TOKEN="$( \
-    curl -X POST -G ${API_URL}/public/api-keys/token \
+    curl -X POST -G ${API_URL}/auth/token \
     -H 'x-api-key: '${API_KEY}'' \
     -H 'client-id: '${CLIENT_ID}'' \
     -H 'scopes: '${SCOPES}'' \
     | jq -r '.token')"
-printf 'token %s /n' "$AUTHORIZATION_TOKEN"
+
   # Wait until the test run has finished
   TOTAL_ITERATION=200
   I=1
